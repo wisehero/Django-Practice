f607e4024a3e4199e3ad80709a3cb787ee6afd92
wisehero10 <godjw96@gmail.com>
2022-11-28 18:24:00 +0900

    # 클라이언트 요청 처리와 URL 매핑

--- mydjango01/app/views.py
+++ mydjango01/app/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from app.models import Post
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+    return render(
+        request,
+        "app/index.html",
+        {
+            "post_list": qs,
+        },
+    )
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = Post.objects.get(pk=pk)
+    return render(request, "app/post_detail.html", {"post": post})
--- mydjango01/mysite/urls.py
+++ mydjango01/mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views as app_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("app/", app_views.index),
+    path("app/<int:pk>", app_views.post_detail),
 ]
